c883bb7dd3b4119724a4caec1e186ac89b4c7460
Maksim Klippa <Maksim_Klippa@epam.com>
2020-12-28 19:02:39 +0300

    edit

---
+++ src/com/mklippa/algorithms/RandomizeArray.java
@@ -0,0 +1,18 @@
+package com.mklippa.algorithms;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class RandomizeArray {
+    public List<Integer> solve(List<Integer> arr) {
+        Random rnd = new Random();
+        int len = arr.size();
+        for (int i = 0; i < len - 1; i++) {
+            // rnd.nextInt((max - min) + 1) + min
+            int j = rnd.nextInt(len);
+            Collections.swap(arr, i, j);
+        }
+        return arr;
+    }
+}
