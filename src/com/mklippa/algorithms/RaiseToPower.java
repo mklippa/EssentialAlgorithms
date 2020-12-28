027c7cf83ebccfb716d71870320692aa5c7caea5
Maksim Klippa <Maksim_Klippa@epam.com>
2020-12-28 19:56:51 +0300

    power 2

--- src/com/mklippa/algorithms/RaiseToPower.java
+++ src/com/mklippa/algorithms/RaiseToPower.java
@@ -1,6 +1,7 @@
 package com.mklippa.algorithms;
 
 public class RaiseToPower {
+
     public double solve(double value, long power) {
         if (power < 0) {
             value = 1 / value;
@@ -15,5 +16,19 @@ public class RaiseToPower {
         double res = power(value, power / 2);
         if (power % 2 == 0) return res * res;
         return res * res * value;
+    }
+
+    public double solve2(double value, int power) {
+        if (power < 0) {
+            power = -power;
+            value = 1 / value;
+        }
+        double ans = 1;
+        double cur = value;
+        for (long i = power; i > 0; i /= 2) {
+            if (i % 2 == 1) ans *= cur;
+            cur *= cur;
+        }
+        return ans;
     }
 }
