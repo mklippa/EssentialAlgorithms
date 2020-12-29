d176e317a90719f85025fecb4a4bfa7fffd9a5ef
Maksim Klippa <Maksim_Klippa@epam.com>
2020-12-29 12:01:19 +0300

    is prime

---
+++ src/com/mklippa/algorithms/IsPrime.java
@@ -0,0 +1,16 @@
+package com.mklippa.algorithms;
+
+import java.util.Random;
+
+public class IsPrime {
+    private final Random rand = new Random();
+
+    public boolean solve(int p, int maxTests) {
+        for (int i = 0; i < maxTests; i++) {
+            int n = rand.nextInt(p - 1) + 1;
+            if (Math.pow(n, p-1) % p != 1)
+                return false;
+        }
+        return true;
+    }
+}
