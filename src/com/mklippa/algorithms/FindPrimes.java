cba3eac54302d6ed6728dd87088669c791ca6077
Maksim Klippa <Maksim_Klippa@epam.com>
2020-12-29 11:22:13 +0300

    find primes

---
+++ src/com/mklippa/algorithms/FindPrimes.java
@@ -0,0 +1,34 @@
+package com.mklippa.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindPrimes {
+    // O(N*log(log(N)))
+    List<Integer> solve(int maxNumber) {
+        boolean[] isComposite = new boolean[maxNumber+1];
+
+        for (int i = 4; i <= maxNumber; i += 2) {
+            isComposite[i] = true;
+        }
+
+        int nextPrime = 3;
+        int stopAt = (int)Math.sqrt(maxNumber);
+        while (nextPrime <= stopAt) {
+            for (int i = nextPrime*2; i <= maxNumber; i += nextPrime) {
+                isComposite[i] = true;
+            }
+            nextPrime += 2;
+            while (nextPrime <= maxNumber && isComposite[nextPrime])
+                nextPrime += 2;
+        }
+
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= maxNumber; i++) {
+            if (!isComposite[i])
+                primes.add(i);
+        }
+
+        return primes;
+    }
+}
