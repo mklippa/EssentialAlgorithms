59235148a6db4097279d99e862efe70d3d60ec7f
Maksim Klippa <Maksim_Klippa@epam.com>
2020-12-30 11:28:28 +0300

    exponentiate

--- src/com/mklippa/algorithms/RaiseToPower.java
+++ src/com/mklippa/algorithms/RaiseToPower.java
@@ -1,6 +1,38 @@
 package com.mklippa.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RaiseToPower {
+    public int exponentiate(int value, int exp) {
+        List<Integer> powers = new ArrayList<>();
+        List<Integer> valueToPowers = new ArrayList<>();
+
+        int lastPower = 1;
+        int lastValue = value;
+
+        powers.add(lastPower);
+        valueToPowers.add(lastValue);
+
+        while (lastPower < exp) {
+            lastPower *= 2;
+            lastValue *= lastValue;
+            powers.add(lastPower);
+            valueToPowers.add(lastValue);
+        }
+
+        int result = 1;
+
+        for (int powerIndex = powers.size()-1; powerIndex >= 0; powerIndex--) {
+            if (powers.get(powerIndex) <= exp) {
+                exp -= powers.get(powerIndex);
+                result *= valueToPowers.get(powerIndex);
+            }
+        }
+
+        return result;
+    }
+
     // O(log(power))
     public double solve(double value, long power) {
         if (power < 0) {
