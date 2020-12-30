3b56e1bbb75bed939852a70b840f1301122ab642
Maksim Klippa <Maksim_Klippa@epam.com>
2020-12-30 15:51:29 +0300

    finding

--- src/com/mklippa/algorithms/linkedLists/Cell.java
+++ src/com/mklippa/algorithms/linkedLists/Cell.java
@@ -1,6 +1,11 @@
 package com.mklippa.algorithms.linkedLists;
 
 public class Cell<T> {
-    public int value;
+    public T value;
     public Cell<T> next;
+
+    @Override
+    public String toString() {
+        return String.format("Value: %s", value);
+    }
 }
